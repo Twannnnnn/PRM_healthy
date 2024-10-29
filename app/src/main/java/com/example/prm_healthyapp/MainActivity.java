517472912939bc252bb0f247fdc1033ceadf4b59
Twannnnnn <hkt93885@gmail.com>
2024-10-29 13:50:42 +0700

    recommit

--- app/src/main/java/com/example/prm_healthyapp/MainActivity.java
+++ app/src/main/java/com/example/prm_healthyapp/MainActivity.java
@@ -114,4 +114,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, SleepLogListActivity.class);
         startActivity(intent);
     }
+
+    
 }
